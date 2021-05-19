29a154037f34acb7c2d0444d730e0d91da103e41
Bezrukov Yuriy <bezrukov-yra@mail.ru>
2021-05-19 19:39:44 +0300

    Create mail-ru-selectors.js

---
+++ mail-ru-selectors.js
@@ -0,0 +1,9 @@
+function getLoginSelector() {
+    return '[data-testid="login-input"]';
+}
+function getPasswordSelector() {
+    return '[data-testid="password-input"]';
+}
+function getSubmitSelector() {
+    return 'data-testid="login-to-mail"';
+}
